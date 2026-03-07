2b54c0ccc18a49e65ed4cb2d48910ded67af97c0
Lucas Henrique Jurgensen <lucas_jurgensen@hotmail.com>
2026-03-06 22:31:23 -0300

    added page manager to manage the permissions | fix css

---
+++ public/assets/pageManager.js
@@ -0,0 +1,59 @@
+class User {
+    constructor(name,level){
+        this.name = name;
+        this.level =  level;
+    }
+
+    levelManagement(levelRequired){
+        const levels = {
+            'admin': 3,
+            'operator': 2,
+            'user': 1
+        };
+
+        if (!levelRequired) return true;
+        
+        return levels[this.level] >= levels[levelRequired];
+
+    }
+
+}
+
+class PermissionManager{
+    constructor(user){
+        this.user = user;
+    }
+
+    applyRestrictions(){
+        const restrictedElements = document.querySelectorAll('[data-nivel]');
+
+        restrictedElements.forEach(element => {
+            const levelRequired = element.getAttribute('data-nivel');
+
+            if(!this.user.levelManagement(levelRequired)){
+
+                element.classList.add('oculto');
+            }
+        });
+
+        this.refreshPage();
+    }
+
+    refreshPage(){
+        
+        const sideElement = document.querySelector('.text-secondary[style*="0.75rem"]');
+
+        if (sideElement){
+            
+            sideElement.textContent = this.user.level.charAt(0).toUpperCase() + this.user.level.slice(1);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const currentUser = new User('Pedro', 'operator');
+
+    const uiManager = new PermissionManager(currentUser);
+    uiManager.applyRestrictions();
+})
