87d49f0bfcb4b57ff545461cd3d5d61792af0e08
Lucas Henrique Jurgensen <lucas_jurgensen@hotmail.com>
2026-03-12 15:03:26 -0300

    feat (password-reset pop-up): added password reset pop up in login page and fixed responsivity in tickets page for laptops or other devices

---
+++ public/assets/login.js
@@ -0,0 +1,13 @@
+function showPasswordReset() {
+    document.getElementById('password-reset').classList.remove('oculto');
+}
+
+function closePasswordReset() {
+    document.getElementById('password-reset').classList.add('oculto');
+}
+
+function passwordReset(event) {
+    event.preventDefault();
+    alert('E-mail de recuperação enviado!');
+    closePasswordReset(); 
+}
