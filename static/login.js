cd75d39f140b01548f2cdbfface20a3382d8194d
Lucas Henrique Jurgensen <lucas_jurgensen@hotmail.com>
2026-05-15 22:40:54 -0300

    feat(password-reset): added modals to reset the users passwords in login page

--- static/login.js
+++ static/login.js
@@ -4,12 +4,34 @@ function showPasswordReset() {
 
 function closePasswordReset() {
     document.getElementById('password-reset').classList.add('oculto');
+    document.getElementById('reset-modal-overlay').classList.add('oculto');
 }
 
-function passwordReset(event) {
-    event.preventDefault();
-    alert('E-mail de recuperação enviado!');
-    closePasswordReset(); 
+function passwordReset() {
+  const senha = document.getElementById('nova_senha').value;
+  const confSenha = document.getElementById('confirma_senha').value;
+
+  if (senha === confSenha){
+    alert('Senha alterada com sucesso!');
+    closePasswordReset();
+  } else {
+    alert('O conteúdo dos campos não coincidem! Tente novamente!');
+  }
+
+}
+
+function checkEmail(){
+    
+    const email = 'suporte@gmail.com';
+
+    const userEmail = document.getElementById('e-mail').value;
+
+    if (email == userEmail){
+        document.getElementById('reset-modal-overlay').classList.remove('oculto');
+    } else {
+        alert('E-mail invalido! Por favor, tente novamente.');
+    }
+
 }
 
 
